de673528edec6f9cceca047dada0e1f4a9c423e0
Akash1321 <Sarkarakash992@gmail.com>
2023-05-20 02:19:32 +0530

    feat: added filter logic

--- fret-house/src/pages/products/Products.jsx
+++ fret-house/src/pages/products/Products.jsx
@@ -1,14 +1,13 @@
 import {useState} from "react";
 
-import { useData } from "../../context/DataContext";
+import {useFilter} from "../../context/FilterContext";
 import ProductCard from "../../components/productCard/ProductCard";
 import ProductFilter from "./components/filters/ProductFilter";
 import "./Products.css";
 
 const Products = () => {
   const [view, setView] = useState({ filters: false, types: false });
-
-  const { state } = useData();
+  const {filterProduct, productsSorted} = useFilter();
 
   const handleFilterView = () => {
     setView(prev => ({...prev, filters: !prev.filters}))
@@ -18,17 +17,28 @@ const Products = () => {
     setView((prev) => ({ ...prev, types: !prev.types }));
   };
 
+  const handleInStock = (e) => {
+    // console.log(e.target.checked)
+    filterProduct({type:"ONLY_INSTOCK", payload: e.target.checked})
+  }
+
 
   return (
     <main className="container products-container">
+
       <ProductFilter view={view} handleFilterView={handleFilterView} handleTypesView={handleTypesView}/>
+
       <header className="section-header">
-        <h1 className="fs-heading fw-bold products-heading">All Products</h1>
+        <label className="fw-semiBold text-primary-400">
+          <input type="checkbox" className="all-products" onChange={handleInStock}/>
+            Show All
+          </label>
+         <h1 className="fs-heading fw-bold products-heading">All Products</h1>
         <button className="accent-button filters-button" onClick={handleFilterView}>Filters</button>
       </header>
 
       <ul className="product-list">
-        {state?.products?.map((product) => (
+        {productsSorted?.map((product) => (
           <ProductCard key={product.id} {...product} />
         ))}
       </ul>
--- fret-house/src/pages/products/components/filters/ProductFilter.jsx
+++ fret-house/src/pages/products/components/filters/ProductFilter.jsx
@@ -1,61 +1,104 @@
 import { X, Plus, Minus } from "react-feather";
 
 import "./ProductFilter.css";
+import {useFilter} from "../../../../context/FilterContext";
 
 const ProductFilter = ({view, handleTypesView, handleFilterView}) => {
+
+    const {filterProduct} = useFilter();
+
+    const ratingArray = [4, 3, 2, 1];
+
+    const handleRadioFilter = (ratingSelected) => {
+        filterProduct({type: "RATING_FILTER", payload: ratingSelected})
+    }
+
+    const handleCategoryFilter = (e) => {
+        filterProduct({type: "CATEGORY_FILTER", payload: e.target.value})
+        console.log(e.target.value)
+    }
+
+    const handlePriceRange = (e) => {
+        filterProduct({type: "PRICE_FILTER", payload: e.target.value})
+    }
+
+    const handlePriceSorting = (e) => {
+        filterProduct({type: "PRICE_SORTING", payload: e.target.value})
+    }
+
+    const handleClearFilters = () => {
+        filterProduct({type: "CLEAR_FILTERS"})
+
+    }
+    
   return (
     <>
-    {view.filters && (<div className="filter-container text-primary-400">
+    <div className="filter-container text-primary-400" style={{display: view.filters ? "flex" : "none"}}>
       <X className="filter-view" onClick={handleFilterView}/>
       <div className="category-choice">
         <h2>TYPES OF PRODUCT</h2>
         <span onClick={handleTypesView}>{view.types ? <Minus /> : <Plus />}</span>
         </div>
-        {view.types && (
-          <div className="category-options">
+    
+          <div className={view.types ? "category-options" : "hideItem"}>
             <div className='separate-option'>
-              <label className="option-label"><input type="checkbox" />ACOUSTIC</label>
-              <label className="option-label"><input type="checkbox" />ELECTRIC</label>
+              <label className="option-label">
+                 <input type="checkbox" value="acoustic" onChange={handleCategoryFilter} />ACOUSTIC
+               </label>
+              <label className="option-label">
+                 <input type="checkbox" value="electric" onChange={handleCategoryFilter} />ELECTRIC
+              </label>
             </div>
             <div className='separate-option'>
-              <label className="option-label"><input type="checkbox" />BASS</label>
-              <label className="option-label"><input type="checkbox" />ACCESSORIES</label>
+              <label className="option-label">
+                 <input type="checkbox" value="bass" onChange={handleCategoryFilter} />BASS
+              </label>
+              <label className="option-label">
+                 <input type="checkbox" value="accessories" onChange={handleCategoryFilter} />ACCESSORIES 
+              </label>
             </div>
              
           </div>
-        )}
       
 
       <div className="rating-choice">
         <h2>RATING</h2>
         <div>
-           <label><input type="radio" name="rating"/>4+</label>
-           <label><input type="radio" name="rating"/>3+</label>
-           <label><input type="radio" name="rating"/>2+</label>
-           <label><input type="radio" name="rating"/>1+</label>
+            {ratingArray.map(rating => (
+            <label key={rating}>
+                <input type="radio" name='rating' onChange={() => handleRadioFilter(rating)}/>{rating}+
+            </label>
+            ))}
         </div>
       </div>
 
       <div className="range">
         <h2>PRICE RANGE</h2>
-        <div><input type="range" /> </div>
+        <div><input type="range" min="50" max="100000" steps="100" onChange={handlePriceRange}/> </div>
       </div>
 
       <div className="price-sorting">
         <h2 >SORT WITH PRICE</h2>
         <div>
           <div>
-             <label><input type="radio" name="sort-price"/>HIGH TO LOW</label>
-             <label><input type="radio" name="sort-price"/>LOW TO HIGH</label>
+
+            <label>
+                <input type="radio" name="sort-price" value="HIGH_TO_LOW" onChange={handlePriceSorting}/>HIGH TO LOW
+            </label>
+
+            <label>
+                <input type="radio" name="sort-price" value="LOW_TO_HIGH" onChange={handlePriceSorting}/>LOW TO HIGH
+            </label>
+
           </div>
         </div>
       </div>
 
       <div className="label-clear">
         <div>PRODUCT 40</div>
-        <button className="accent-button">CLEAR</button>
+        <button className="accent-button" onClick={handleClearFilters}>CLEAR</button>
       </div>
-    </div>)}
+    </div>
     </>
   );
 };
