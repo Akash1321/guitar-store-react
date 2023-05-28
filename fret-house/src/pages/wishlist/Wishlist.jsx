95fcd45ca62093e433a56860c23f9ef25423ecf6
Akash1321 <Sarkarakash992@gmail.com>
2023-05-29 02:37:19 +0530

    feat: empty wishlist

--- fret-house/src/pages/wishlist/Wishlist.jsx
+++ fret-house/src/pages/wishlist/Wishlist.jsx
@@ -1,24 +1,50 @@
-// import "./Products.css"
-import {useData} from "../../context/DataContext";
+import { useNavigate } from "react-router-dom";
+
+import "./Wishlist.css";
+import { useData } from "../../context/DataContext";
 import ProductCard from "../../components/productCard/ProductCard";
 
 const Wishlist = () => {
-    const {state} = useData();
+  const {
+    state: { wishList },
+  } = useData();
+  const navigate = useNavigate();
 
-    return (
-        <main className="container products-container">
-    
-          <header className="section-header">
-             <h1 className="fs-heading fw-bold products-heading">All Products</h1>
-          </header>
-    
-          <ul className="product-list">
-            {state?.wishList.map((product) => (
-              <ProductCard key={product.id} {...product} />
-            ))}
-          </ul>
-        </main>
-      );
-}
+  const handleExploreProducts = () => {
+    navigate("/products");
+  };
+
+  return (
+    <main className="container products-container">
+
+      <header className="section-header">
+
+        <h1 className="fs-heading fw-bold products-heading">Wishlist</h1>
+
+        {wishList.length === 0 && (
+          <div className="empty-wishlist">
+            <p className="fw-semiBold text-primary-400 fs-heading">
+              No Items Added
+            </p>
+            <button
+              className="primary-button explore-button"
+              onClick={handleExploreProducts}
+            >
+              Explore Products
+            </button>
+          </div>
+        )}
+        
+      </header>
+
+      <ul className="product-list">
+        {wishList.map((product) => (
+          <ProductCard key={product.id} product={product} wishListCard />
+        ))}
+      </ul>
+
+    </main>
+  );
+};
 
 export default Wishlist;
