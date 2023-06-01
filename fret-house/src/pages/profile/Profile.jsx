bc19fc87cfc15d9af158f843f996da7e0949a1b4
Akash1321 <Sarkarakash992@gmail.com>
2023-06-01 12:14:44 +0530

    style: profile page

--- fret-house/src/pages/profile/Profile.jsx
+++ fret-house/src/pages/profile/Profile.jsx
@@ -1,15 +1,20 @@
 import {useAuth} from "../../context/AuthContext";
+import "./Profile.css";
 
 const Profile = () => {
 
     const {user, handleLogout} = useAuth();
 
     return(
-        <>
-        <h1 style={{fontWeight: "bold", fontSize: "4rem", marginTop: "8rem"}}>This is Profile Page</h1>
-        <p>{user.firstName ?? user.name}</p>
-        <button className="accent-button" onClick={handleLogout}>LOG OUT</button>
-        </>
+        <div className="container profile-container text-primary-400">
+        <h1 className="fs-heading fw-bold ">Profile</h1>
+        <div className="user-detail">
+        <p><span className="fw-semiBold">Name:</span> {user.firstName ?? user.name} {user.lastName}</p>
+        <p><span className="fw-semiBold">Email:</span> {user.email} </p>
+        </div>
+        
+        <button className="accent-button logout-button" onClick={handleLogout}>LOG OUT</button>
+        </div>
         
     )
 }
