ab4f1d4ee74d8b608679829e709ea1c9dd035b18
Akash1321 <Sarkarakash992@gmail.com>
2023-06-01 12:51:02 +0530

    style: logout page

--- fret-house/src/pages/auth/logout/Logout.jsx
+++ fret-house/src/pages/auth/logout/Logout.jsx
@@ -6,8 +6,7 @@ const Logout = () => {
     const navigate = useNavigate()
     return (
         <div className="container logout text-primary-400 logout-display">
-        <h1 className="fs-heading fw-bold">You are logged out</h1>
-        <p>View Products</p>
+        <h1 className="fs-heading fw-bold logout-text">You are logged out</h1>
         <button className="primary-button" onClick={() => navigate("/products")}>Go to Products</button>
         </div>
     )
